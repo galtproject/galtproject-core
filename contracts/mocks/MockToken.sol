c0c0aac042a7f7c9b3e4c323dc656693bf92d4c7
Nikita Chebykin <nikita@chebyk.in>
2018-07-31 14:01:18 +0200

    Add proxy implementation

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -2,22 +2,28 @@ pragma solidity 0.4.24;
 pragma experimental "v0.5.0";
 
 import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+// TODO: remove initializer
+import "zos-lib/contracts/migrations/Initializable.sol";
 
 
-contract GaltToken is MintableToken {
+contract MockToken is Initializable, MintableToken {
   // solium-disable-next-line uppercase
-  string public constant name = "Galt Token";
+  string public constant name = "Mock Token";
 
   // solium-disable-next-line uppercase
-  string public constant symbol = "GALT";
+  string public constant symbol = "MALT";
 
   // solium-disable-next-line uppercase
   uint8 public constant decimals = 18;
 
   uint256 public constant INITIAL_SUPPLY = 0;
 
-  constructor() public MintableToken() {
-    // TODO: figure out how the owner is assigned
+  constructor() public {
+  }
+
+  function initialize() isInitializer public {
+    owner = msg.sender;
+
     totalSupply_ = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
     emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,30 @@
-/* eslint-disable no-empty-function, no-unused-vars */
+const GaltToken = artifacts.require('./GaltToken');
+const SpaceToken = artifacts.require('./SpaceToken');
+const LandUtils = artifacts.require('./LandUtils');
+const AdminUpgradeabilityProxy = artifacts.require('zos-lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol');
 
-module.exports = async function(deployer) {};
+module.exports = async function(deployer, network, accounts) {
+  if (network === 'test') {
+    return;
+  }
+
+  const coreTeam = accounts[0];
+  const proxiesAdmin = accounts[1];
+
+  // Deploy contracts...
+  await deployer.deploy(GaltToken, { from: coreTeam });
+  await deployer.deploy(SpaceToken, 'Space Token', 'SPACE', { from: coreTeam });
+  await deployer.deploy(LandUtils, { from: coreTeam });
+
+  // Setup proxies...
+  const spaceProxy = await AdminUpgradeabilityProxy.new(SpaceToken.address, { from: proxiesAdmin });
+  const spaceToken = await SpaceToken.at(spaceProxy.address);
+  spaceToken.initialize('Space Token', 'SPACE', { from: coreTeam });
+
+  const landUtilsProxy = await AdminUpgradeabilityProxy.new(LandUtils.address, { from: proxiesAdmin });
+  const landUtils = await LandUtils.at(landUtilsProxy.address);
+  landUtils.initialize({ from: coreTeam });
+
+  console.log('SpaceToken Proxy:', spaceProxy.address);
+  console.log('LandUtils Proxy:', landUtils.address);
+};
