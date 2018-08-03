fef99110468ee1d22565c98d463ca36813734606
Nikita Chebykin <nikita@chebyk.in>
2018-08-03 18:09:04 +0200

    Add SpaceToken masks

--- contracts/SpaceToken.sol
+++ contracts/SpaceToken.sol
@@ -8,10 +8,23 @@ import "zos-lib/contracts/migrations/Initializable.sol";
 // Just keep it here to make it loaded in tests
 import "zos-lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol";
 
-
+/*
+ * SpaceToken id encode the following additional logic
+ * left two bits describes what type is it:
+ *  - 0x01 for geohash, for ex. "0x01f9388f000000000...0000
+ *  - 0x02 for pack, for ex. "0x020000000000000000...0003022fd0"
+ *
+ */
 contract SpaceToken is ERC721Token, Ownable, Initializable {
   bytes4 private constant InterfaceId_ERC721Enumerable = 0x780e9d63;
   bytes4 private constant InterfaceId_ERC721Metadata = 0x5b5e139f;
+
+  bytes32 public constant GeohashMask = 0x0100000000000000000000000000000000000000000000000000000000000000;
+  bytes32 public constant PackMask = 0x0200000000000000000000000000000000000000000000000000000000000000;
+
+  uint256 packTokenIdCounter;
+
+  event SpaceTokenMinted(bytes32 id, address owner);
 
   constructor(
     string name,
@@ -22,12 +35,14 @@ contract SpaceToken is ERC721Token, Ownable, Initializable {
   {
   }
 
-  function initialize(string _name, string _symbol) isInitializer public {
+  function initialize(address _owner, string _name, string _symbol) isInitializer public {
     // TODO: figure out how to call constructor
     // For now all parent constructors code is copied here
-    owner = msg.sender;
+    owner = _owner;
     name_ = _name;
     symbol_ = _symbol;
+
+    packTokenIdCounter = 0;
 
     // register the supported interfaces to conform to ERC721 via ERC165
     _registerInterface(InterfaceId_ERC721Enumerable);
@@ -36,13 +51,36 @@ contract SpaceToken is ERC721Token, Ownable, Initializable {
 
   function mint(
     address _to,
-    address _toFund,
-    uint256 _tokenId,
-    uint256 _amount
+    uint256 _tokenId
   )
     public
   {
     super._mint(_to, _tokenId);
+    emit SpaceTokenMinted(bytes32(_tokenId), _to);
+  }
+
+  function mintPack(
+    address _to
+  )
+    public
+    onlyOwner
+    returns (uint256 _tokenId)
+  {
+    _tokenId = generatePackTokenId();
+    mint(_to, _tokenId);
+  }
+
+  function generatePackTokenId() internal returns (uint256) {
+    bytes32 newIdBytes = bytes32(packTokenIdCounter++);
+
+    // Do not allow create more than 2^62 (4.611e18) packs
+    assert((newIdBytes & PackMask) == 0x0);
+
+    uint256 newId = uint256(newIdBytes ^ PackMask);
+
+    assert(!exists(newId));
+
+    return newId;
   }
 
   function burn(uint256 _tokenId) public {
@@ -51,5 +89,13 @@ contract SpaceToken is ERC721Token, Ownable, Initializable {
 
   function setTokenURI(uint256 _tokenId, string _uri) public {
     super._setTokenURI(_tokenId, _uri);
+  }
+
+  function isGeohash(bytes32 id) pure public returns (bool) {
+    return (id & GeohashMask) == GeohashMask;
+  }
+
+  function isPack(bytes32 id) pure public returns (bool) {
+    return (id & PackMask) == PackMask;
   }
 }
