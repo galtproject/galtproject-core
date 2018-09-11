ca5cb3fb66ece9531031542589dec37ce5cbed98
Nikita Chebykin <nikita@chebyk.in>
2018-09-11 23:31:56 +0200

    Increase default test balance to 5000 ETH

--- truffle.js
+++ truffle.js
@@ -22,7 +22,7 @@ const config = {
       provider: Ganache.provider({
         unlocked_accounts: [0, 1, 2, 3, 4, 5],
         vmErrorsOnRPCResponse: true,
-        default_balance_ether: 500,
+        default_balance_ether: 5000,
         // 7 800 000
         gasLimit: 0x7704c0
       }),
