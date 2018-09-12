c1ea806635714f368d7552cf18af14127cbfa6f9
jonybang <Jonybange@gmail.com>
2018-09-12 14:58:49 +0200

    truffle.js

--- truffle.js
+++ truffle.js
@@ -8,6 +8,16 @@ const config = {
       port: 8545,
       network_id: '*'
     },
+    ganache: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*'
+    },
+    testnet57: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*'
+    },
     development: {
       host: '127.0.0.1',
       port: 7545,
