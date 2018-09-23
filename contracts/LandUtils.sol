7a9e01b306474e2e6455c017598c1ad71b73ea90
Nikita Chebykin <nikita@chebyk.in>
2018-09-23 11:59:14 +0200

    Add geohash capacity evaluation util

--- contracts/LandUtils.sol
+++ contracts/LandUtils.sol
@@ -3,9 +3,51 @@ pragma experimental "v0.5.0";
 
 import "zos-lib/contracts/migrations/Initializable.sol";
 
+library LandUtils {
+  uint256 constant C1_GEOHASH = 31;
+  uint256 constant C2_GEOHASH = 1023;
+  uint256 constant C3_GEOHASH = 32767;
+  uint256 constant C4_GEOHASH = 1048575;
+  uint256 constant C5_GEOHASH = 33554431;
+  uint256 constant C6_GEOHASH = 1073741823;
+  uint256 constant C7_GEOHASH = 34359738367;
+  uint256 constant C8_GEOHASH = 1099511627775;
+  uint256 constant C9_GEOHASH = 35184372088831;
+  uint256 constant C10_GEOHASH = 1125899906842623;
+  uint256 constant C11_GEOHASH = 36028797018963967;
+  uint256 constant C12_GEOHASH = 1152921504606846975;
 
-// solium-disable-next-line no-empty-blocks
-contract LandUtils is Initializable {
-  function initialize() public isInitializer {
+  function geohash5Capacity(uint256 _geohash5) external view returns (uint256) {
+    if (_geohash5 == 0) {
+      revert("Invalid geohash5");
+    } else if (_geohash5 <= C1_GEOHASH) {
+      return 1;
+    } else if (_geohash5 <= C2_GEOHASH) {
+      return 2;
+    } else if (_geohash5 <= C2_GEOHASH) {
+      return 2;
+    } else if (_geohash5 <= C3_GEOHASH) {
+      return 3;
+    } else if (_geohash5 <= C4_GEOHASH) {
+      return 4;
+    } else if (_geohash5 <= C5_GEOHASH) {
+      return 5;
+    } else if (_geohash5 <= C6_GEOHASH) {
+      return 6;
+    } else if (_geohash5 <= C7_GEOHASH) {
+      return 7;
+    } else if (_geohash5 <= C8_GEOHASH) {
+      return 8;
+    } else if (_geohash5 <= C9_GEOHASH) {
+      return 9;
+    } else if (_geohash5 <= C10_GEOHASH) {
+      return 10;
+    } else if (_geohash5 <= C11_GEOHASH) {
+      return 11;
+    } else if (_geohash5 <= C12_GEOHASH) {
+      return 12;
+    } else {
+      revert("Invalid geohash5");
+    }
   }
 }
---
+++ test/unit/testLandUtils.js
@@ -0,0 +1,55 @@
+const LandUtils = artifacts.require('./LandUtils.sol');
+const Web3 = require('web3');
+const { assertRevert } = require('../helpers');
+
+const web3 = new Web3(LandUtils.web3.currentProvider);
+
+contract('LandUtils', ([deployer]) => {
+  beforeEach(async function() {
+    this.utils = await LandUtils.new({ from: deployer });
+    this.utils3 = new web3.eth.Contract(this.utils.abi, this.utils.address);
+  });
+
+  describe('#geohash5Capacity()', () => {
+    it('provide correct results', async function() {
+      assert.equal(await this.utils3.methods.geohash5Capacity('1').call(), 1);
+      assert.equal(await this.utils3.methods.geohash5Capacity('15').call(), 1);
+      assert.equal(await this.utils3.methods.geohash5Capacity('31').call(), 1);
+      assert.equal(await this.utils3.methods.geohash5Capacity('32').call(), 2);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1023').call(), 2);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1024').call(), 3);
+      assert.equal(await this.utils3.methods.geohash5Capacity('32767').call(), 3);
+      assert.equal(await this.utils3.methods.geohash5Capacity('32768').call(), 4);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1048575').call(), 4);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1048576').call(), 5);
+      assert.equal(await this.utils3.methods.geohash5Capacity('33554431').call(), 5);
+      assert.equal(await this.utils3.methods.geohash5Capacity('33554432').call(), 6);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1073741823').call(), 6);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1073741824').call(), 7);
+      assert.equal(await this.utils3.methods.geohash5Capacity('34359738367').call(), 7);
+      assert.equal(await this.utils3.methods.geohash5Capacity('34359738368').call(), 8);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1099511627775').call(), 8);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1099511627776').call(), 9);
+      assert.equal(await this.utils3.methods.geohash5Capacity('35184372088831').call(), 9);
+      assert.equal(await this.utils3.methods.geohash5Capacity('35184372088832').call(), 10);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1125899906842623').call(), 10);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1125899906842624').call(), 11);
+      assert.equal(await this.utils3.methods.geohash5Capacity('36028797018963967').call(), 11);
+      assert.equal(await this.utils3.methods.geohash5Capacity('36028797018963968').call(), 12);
+      assert.equal(await this.utils3.methods.geohash5Capacity('1152921504606846975').call(), 12);
+    });
+
+    it('should revert on 0 input', async function() {
+      await assertRevert(this.utils3.methods.geohash5Capacity('0').call());
+    });
+
+    it('should revert on empty input', async function() {
+      await assertRevert(this.utils3.methods.geohash5Capacity('0').call());
+    });
+
+    it('should revert if a value is greater than max', async function() {
+      // max is 1152921504606846975
+      await assertRevert(this.utils3.methods.geohash5Capacity('1152921504606846976').call());
+    });
+  });
+});
