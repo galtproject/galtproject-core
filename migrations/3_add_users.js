d3b45d901ea2df3dc5b044b310419c3f14991c61
jonybang <Jonybange@gmail.com>
2018-10-26 12:39:27 +0200

    add users and mint space tokens on deploy

--- migrations/3_add_users.js
+++ migrations/3_add_users.js
@@ -1,6 +1,8 @@
 const PlotManager = artifacts.require('./PlotManager');
 const PlotValuation = artifacts.require('./PlotValuation');
 const PlotCustodian = artifacts.require('./PlotCustodianManager');
+const SpaceToken = artifacts.require('./SpaceToken');
+const SplitMerge = artifacts.require('./SplitMerge');
 const GaltToken = artifacts.require('./GaltToken');
 const GaltDex = artifacts.require('./GaltDex');
 const SpaceDex = artifacts.require('./SpaceDex');
@@ -14,6 +16,7 @@ const web3 = new Web3(PlotManager.web3.currentProvider);
 
 const fs = require('fs');
 const _ = require('lodash');
+const galt = require('@galtproject/utils');
 
 function ether(value) {
   return Web3.utils.toWei(value.toString(10), 'ether');
@@ -35,6 +38,8 @@ module.exports = async function(deployer, network, accounts) {
     const plotManager = await PlotManager.at(data.plotManagerAddress);
     const plotValuation = await PlotValuation.at(data.plotValuationAddress);
     const plotCustodian = await PlotCustodian.at(data.plotCustodianAddress);
+    const spaceToken = await SpaceToken.at(data.spaceTokenAddress);
+    const splitMerge = await SplitMerge.at(data.splitMergeAddress);
     const galtToken = await GaltToken.at(data.galtTokenAddress);
     const galtDex = await GaltDex.at(data.galtDexAddress);
     const spaceDex = await SpaceDex.at(data.spaceDexAddress);
@@ -107,6 +112,8 @@ module.exports = async function(deployer, network, accounts) {
       Jonybang2: '0x7DB143B5B2Ef089992c89a27B015Ab47391cdfFE',
       Jonybang3: '0x029001F6C2dc2C8B28Ff201C3a451714637Af4E3',
       Nikita: '0x8d362af4c86b05d6F256147A6E76b9d7aF205A24',
+      Nikita2: '0x41a19AFBa5184ae7fE2607dF5895082f2056912B',
+      Nikita3: '0xf8802036a4Cc35aaDC5aaa3fAE15935282D2F7c7',
       Igor: '0x06dba6eb6a1044b8cbcaa0033ea3897bf37e6671',
       Igor2: '0x8052C9fc345dB9c1A70Afc0A81416029F23E5f76',
       Igor3: '0xF6310635508a46ba66AaBdf28486744c715d6bcC',
@@ -117,10 +124,20 @@ module.exports = async function(deployer, network, accounts) {
       Nik5: '0xa583ac4bD05794a9BBCc64Aa3c853529A0eCA996',
       NickUser: '0x7184e0fF3c8D6FC24B986177c131290A0a7A9B28',
       NickValidator: '0x82a79ccdDFf049bE2715621c3CD17a6A4BaFC099',
-      NickAdmin: '0x8EE35beC646E131e07ece099c2Eb2697d0a588D5'
+      NickAdmin: '0x8EE35beC646E131e07ece099c2Eb2697d0a588D5',
+      DevNickUser: '0x075c3e0d1a4829c866ea9048a335bd3955e8da33',
+      DevNick: '0x84131ce9f499667c6fd7ec9e0860d8dfaba63ed9',
+      DevNick2: '0xafc0fd8153bd835fa6e57e8b5c5b3210c44c5069',
+      DevNick3: '0xef7751e98c135d28af63d1353cb02dc502b72ee6',
+      DevNick4: '0x02ffe5da61fbf31d46b1d8468487b86109e41943',
+      DevNick5: '0xc953e56acd698e1e7a1c2eb930eb7f53c2153d31',
+      DevNickValidator: '0x3ff14ddd3da95f6f9ae7110c5197834e6167c8a3',
+      DevNickValidator2: '0xa39b23e3befec6711f4c207c84604293f4409193',
+      DevNickValidator3: '0xc25b780c31a93a95d0e0bca3ccc63645f7e7de6a',
+      DevNickAdmin: '0x7c1523a06cf76de0eb49de797f088c7cb40ea9c7'
     };
 
-    const adminsList = ['Jonybang', 'Nikita', 'Igor', 'Nik', 'Nik2', 'NickAdmin'];
+    const adminsList = ['Jonybang', 'Nikita', 'Igor', 'Nik', 'Nik2', 'NickAdmin', 'DevNik', 'DevNik2', 'DevNickAdmin'];
 
     const allRoles = [
       PM_CADASTRAL_ROLE,
@@ -146,7 +163,15 @@ module.exports = async function(deployer, network, accounts) {
       NickValidator: allRoles,
       Nik3: [PV_APPRAISER_ROLE],
       Nik4: [PV_APPRAISER2_ROLE],
-      Nik5: [PV_AUDITOR_ROLE]
+      Nik5: [PV_AUDITOR_ROLE],
+      DevNik: allRoles,
+      DevNik2: allRoles,
+      DevNickValidator: allRoles,
+      DevNickValidator2: allRoles,
+      DevNickValidator3: allRoles,
+      DevNik3: [PV_APPRAISER_ROLE],
+      DevNik4: [PV_APPRAISER2_ROLE],
+      DevNik5: [PV_AUDITOR_ROLE]
     };
 
     const minDepositForValidator = 500; // 10k $
@@ -217,6 +242,32 @@ module.exports = async function(deployer, network, accounts) {
       const sendWei = web3.utils.toWei(sendEthByNetwork[network].toString(), 'ether').toString(10);
       promises.push(web3.eth.sendTransaction({ from: rewarder, to: address, value: sendWei }).catch(() => {}));
     });
+
+    console.log('create space tokens...');
+
+    await spaceToken.mint(coreTeam, { from: coreTeam });
+    let spaceTokenId = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    let contour = ['w24q8xwe6ty4', 'w24q8xqxcvgc', 'w24q8xrpuv5x', 'w24q8xx1su5x', 'w24q8xxh8wr8'];
+    await splitMerge.setPackageContour(spaceTokenId, contour.map(galt.geohashToGeohash5), { from: coreTeam });
+    await splitMerge.setPackageHeights(spaceTokenId, contour.map(() => ether(2)), { from: coreTeam });
+    await splitMerge.setPackageLevel(spaceTokenId, 0, { from: coreTeam });
+    await spaceToken.transferFrom(coreTeam, users.DevNickUser, spaceTokenId, { from: coreTeam });
+
+    await spaceToken.mint(coreTeam, { from: coreTeam });
+    spaceTokenId = '0x0000000000000000000000000000000000000000000000000000000000000001';
+    contour = ['w24q8xwf4uq0', 'w24q8xwfjuk0', 'w24q8xwfvfk0', 'w24q8xwfffq0'];
+    await splitMerge.setPackageContour(spaceTokenId, contour.map(galt.geohashToGeohash5), { from: coreTeam });
+    await splitMerge.setPackageHeights(spaceTokenId, contour.map(() => 0), { from: coreTeam });
+    await splitMerge.setPackageLevel(spaceTokenId, 1, { from: coreTeam });
+    await spaceToken.transferFrom(coreTeam, users.DevNickUser, spaceTokenId, { from: coreTeam });
+
+    await spaceToken.mint(coreTeam, { from: coreTeam });
+    spaceTokenId = '0x0000000000000000000000000000000000000000000000000000000000000002';
+    contour = ['w24q8xwf4uq0', 'w24q8xwfjuk0', 'w24q8xwfvfk0', 'w24q8xwfffq0'];
+    await splitMerge.setPackageContour(spaceTokenId, contour.map(galt.geohashToGeohash5), { from: coreTeam });
+    await splitMerge.setPackageHeights(spaceTokenId, contour.map(() => ether(2)), { from: coreTeam });
+    await splitMerge.setPackageLevel(spaceTokenId, 2, { from: coreTeam });
+    await spaceToken.transferFrom(coreTeam, users.DevNickUser, spaceTokenId, { from: coreTeam });
 
     await Promise.all(promises);
   });
