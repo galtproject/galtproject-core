f4d8d0bb038e1a7c48df35e7298c5bf613fa61f5
jonybang <Jonybange@gmail.com>
2018-11-07 13:10:19 +0200

    link SegmentUtils to test contract

--- test/integration/testSegmentUtils.js
+++ test/integration/testSegmentUtils.js
@@ -1,4 +1,6 @@
 const TestSegmentUtils = artifacts.require('./test/TestSegmentUtils.sol');
+const SegmentUtils = artifacts.require('./test/SegmentUtils.sol');
+
 const Web3 = require('web3');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
@@ -17,12 +19,16 @@ chai.should();
 
 contract('SegmentUtils', ([coreTeam]) => {
   beforeEach(async function() {
+    this.segmentUtils = await SegmentUtils.new({ from: coreTeam });
+    TestSegmentUtils.link('SegmentUtils', this.segmentUtils.address);
     this.testSegmentUtils = await TestSegmentUtils.new({ from: coreTeam });
   });
 
   describe('#segmentsIntersect()', () => {
     it.only('should correctly detect segmentsIntersect', async function() {
-      let res = await this.testSegmentUtils.segmentsIntersect([[-2,-2], [2,2]], [[-1,1], [3,1]], { from: coreTeam });
+      const res = await this.testSegmentUtils.segmentsIntersect([[-2, -2], [2, 2]], [[-1, 1], [3, 1]], {
+        from: coreTeam
+      });
       // assert.equal(res, alice);
     });
   });
