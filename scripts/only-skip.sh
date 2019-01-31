edede3913a14d23611d9432517ccc215aa6074c1
Nikita Chebykin <nikita@chebyk.in>
2019-01-31 19:47:47 +0200

    Fix make scripts

--- scripts/only-skip.sh
+++ scripts/only-skip.sh
@@ -2,6 +2,3 @@
 
 # Add .tmp prefix to integration test files to speedup test runs selected with `.only` mocha modifier
 find ./test/integration -name "test*.js" -exec sh -c 'grep -rhF ".only" "$1" || mv "$1" "${1%.js}.skip"' _ {} \;
-mv ./migrations/2_deploy_contracts.js ./migrations/2_deploy_contracts.skip
-mv ./migrations/2_deploy_contracts.mock ./migrations/2_deploy_contracts.js
-mv ./migrations/3_add_users.js ./migrations/3_add_users.skip
