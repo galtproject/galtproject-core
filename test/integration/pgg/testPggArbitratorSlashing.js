b4985d810e3e5779c143c6d3259c6ed11bca3ba4
Nikita Chebykin <nikita@chebyk.in>
2019-06-01 13:34:05 +0200

    Fix ArbitratorSlashing test

--- test/integration/pgg/testPggArbitratorSlashing.js
+++ test/integration/pgg/testPggArbitratorSlashing.js
@@ -140,6 +140,7 @@ contract('ArbitratorSlashing', accounts => {
       this.pggFactory = await deployPGGFactory(this.ggr, coreTeam);
 
       await this.acl.setRole(bytes32('ARBITRATION_STAKE_SLASHER'), this.claimManager.address, true, { from: coreTeam });
+      await this.acl.setRole(bytes32('PGG_MULTI_SIG_PROPOSER'), this.claimManager.address, true, { from: coreTeam });
       await this.acl.setRole(bytes32('ORACLE_STAKE_SLASHER'), this.claimManager.address, true, { from: coreTeam });
       await this.acl.setRole(bytes32('PGG_REGISTRAR'), this.pggFactory.address, true, { from: coreTeam });
       await this.acl.setRole(bytes32('SPACE_REPUTATION_NOTIFIER'), this.sra.address, true, { from: coreTeam });
